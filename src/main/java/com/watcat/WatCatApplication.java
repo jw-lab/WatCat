53874c0426760b1ffe681dae310fdf375797a98b
jw <jdb0731@naver.com>
2022-03-05 17:40:57 +0900

    first commit

---
+++ src/main/java/com/watcat/WatCatApplication.java
@@ -0,0 +1,13 @@
+package com.watcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/watcat/WatCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
