a7eb5631623a8bed7329601d5910e3f9c1c882fe
dlhdlh-HomeDesk <dlhdlh9969@gmail.com>
2022-03-07 12:48:49 +0900

    김동환 커밋

---
+++ src/main/java/com/watcat/controller/MyPageController.java
@@ -0,0 +1,12 @@
+package com.watcat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyPageController {
+	@RequestMapping("mypage")
+	public String mypage() {
+		return "MyPage";
+	}
+}
