6f86daeb623b2273dfbca9aed4be413876256cdc
jw <jdb0731@naver.com>
2022-03-14 15:29:16 +0900

    영화 검색, 리뷰 목록 구현

--- src/main/java/com/watcat/service/MovieRecommendService.java
+++ src/main/java/com/watcat/service/MovieRecommendService.java
@@ -2,6 +2,8 @@ package com.watcat.service;
 
 import java.util.List;
 
+import com.github.pagehelper.Page;
+import com.watcat.dto.reviewDto;
 import com.watcat.dto.movie.MovieWishDto;
 
 public interface MovieRecommendService {
@@ -11,5 +13,11 @@ public interface MovieRecommendService {
 	void deleteMovieWish(MovieWishDto movieWish) throws Exception;
 
 	List<MovieWishDto> selectMovieWish(MovieWishDto movieWish) throws Exception;
+
+	void insertReview(reviewDto reviewDto) throws Exception;
+
+	Page<reviewDto> selectReviewList(int pageNum) throws Exception;
+
+	reviewDto getReviewDetail(int idx) throws Exception;
 
 }
--- src/main/java/com/watcat/service/MovieRecommendServiceImpl.java
+++ src/main/java/com/watcat/service/MovieRecommendServiceImpl.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.watcat.dto.reviewDto;
 import com.watcat.dto.movie.MovieWishDto;
 import com.watcat.mapper.MovieRecommendMapper;
 
@@ -27,6 +30,24 @@ public class MovieRecommendServiceImpl implements MovieRecommendService {
 	@Override
 	public List<MovieWishDto> selectMovieWish(MovieWishDto movieWish) throws Exception {
 		return movieRecommendMapper.selectMovieWish(movieWish);
+	}
+	//리뷰 등록
+	@Override
+	public void insertReview(reviewDto reviewDto) throws Exception {
+		movieRecommendMapper.insertReview(reviewDto);
+		
+	}
+	//리뷰 목록 -pageHelper 사용
+	@Override
+	public Page<reviewDto> selectReviewList(int pageNum) throws Exception {
+		PageHelper.startPage(pageNum,10);
+		return movieRecommendMapper.selectReviewList();
+	}
+	//리뷰 상세보기 페이지
+	@Override
+	public reviewDto getReviewDetail(int idx) throws Exception {
+
+		return movieRecommendMapper.getReviewDetail(idx);
 	}
 
 }
